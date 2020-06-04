4349d33a350b222107b7d7824bba78519e69f79a
bharathjinka09 <bharathjinka09@gmail.com>
2020-06-04 12:11:20 +0530

    Added zoom effect

---
+++ example_usage.py
@@ -0,0 +1,11 @@
+# Import the class
+
+from zipcode_to_map import ZipcodeToMap
+
+# Instantiate the object
+
+result_map = ZipcodeToMap()
+
+# Provides the map as result
+
+result_map.zipcode_to_map('in','515411')
--- zipcode_to_map.py
+++ zipcode_to_map.py
@@ -7,29 +7,20 @@ Provide a Country code and postal code.
 It will generate a map in a html file based on these details.
 Used pgeocode and folium packages.
 '''
+class ZipcodeToMap:
 
-def zipcode_to_map(country_code, postal_code):
-	country_code = pgeocode.Nominatim(country_code)
+	def zipcode_to_map(self, country_code, postal_code):
+		country_code = pgeocode.Nominatim(country_code)
 
-	zip_code = country_code.query_postal_code(postal_code)
+		zip_code = country_code.query_postal_code(postal_code)
 
-	postal_code = zip_code['postal_code']
-
-	# print(postal_code)
-	# print(zip_code)
-	# print(zip_code['latitude'])
-	# print(zip_code['longitude'])
-
-	multiple_zip_codes = country_code.query_postal_code(["560034", "560083"])
-
-	# print(multiple_zip_codes)
-
-	# print(multiple_zip_codes['latitude'])
+		postal_code = zip_code['postal_code']
 
 
-	m = folium.Map(location=[zip_code['latitude'], zip_code['longitude']], zoom_start=9,
-	                   detect_retina=True, control_scale=False)
+		m = folium.Map(location=[zip_code['latitude'], zip_code['longitude']], zoom_start=12,
+		                   detect_retina=True, control_scale=True)
 
-	m.save(f'{postal_code}-map.html')
-
-zipcode_to_map('in','515411')
+		m.save(f'{postal_code}-map.html')
+		print(f'Map generated for {postal_code} postal code')
+		
+		return f'Map generated for {postal_code} postal code'
